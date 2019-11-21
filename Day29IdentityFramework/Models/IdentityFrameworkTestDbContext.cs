68d6d0df8bbd0b2c9b422c6e69f34e692941411a
Mike <michael005@me.com>
2019-11-21 12:11:22 -0500

    First Commit

---
+++ Day29IdentityFramework/Models/IdentityFrameworkTestDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day29IdentityFramework.Models
+{
+    public class IdentityFrameworkTestDbContext: DbContext
+    {
+        public IdentityFrameworkTestDbContext(DbContextOptions options) : base(options) { }
+
+        DbSet<Presidents> Presidents { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Presidents>().HasData(
+                new Presidents(44, "Barak Obama", 2, DateTime.Parse("01/01/2009")),
+                new Presidents(22, "Grover Cleveland", 1, DateTime.Parse("03/04/1885")),
+                new Presidents(24, "Grover Cleveland", 1, DateTime.Parse("03/04/1893")),
+                new Presidents(38, "Gerald Ford", 1, DateTime.Parse("09/09/1974"))
+               );
+        }
+    }
+}
---
+++ Day29IdentityFramework/Models/Presidents.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day29IdentityFramework.Models
+{
+    public class Presidents
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PresidentId { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [Required]
+
+        public int Terms{ get; set; }
+        public DateTime DateTime { get; }
+        [Required]
+        public DateTime InaugurationDate { get; set; }
+
+        public Presidents()
+        {
+
+        }
+
+        public Presidents(int id, string name, int terms, DateTime inaugurationDate)
+        {
+            PresidentId = id;
+            Name = name;
+            Terms = terms;
+            DateTime = inaugurationDate;
+        }
+    }
+}
